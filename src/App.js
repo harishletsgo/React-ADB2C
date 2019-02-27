5a443f39763fc8082a1bf12def36ae7debdd0908
Harish Prasanna <harishperfect@gmail.com>
2019-02-26 21:22:09 -0500

    Added AD B2C

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Auth from './Auth';
 
 class App extends Component {
+  displayUserInformation() {
+    const auth = new Auth();
+    if (auth.isLoggedIn()) {
+      console.log(auth.currentUser());
+      return (
+        <div>
+          <p>
+            Welcome, {auth.currentUser().firstName} {auth.currentUser().lastName} from {auth.currentUser().city}, {auth.currentUser().country}
+             Your email is {auth.currentUser().emails}
+          </p>
+          <a 
+            className="App-link" 
+            href="/welcome" onClick={() => auth.logout()}
+          >
+            Sign Out
+          </a> 
+        </div>
+      );
+      
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {this.displayUserInformation()}
         </header>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import b2cauth from 'react-azure-adb2c';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+b2cauth.initialize({
+  instance: 'https://login.microsoftonline.com/tfp/', 
+  tenant: 'cmapp2.onmicrosoft.com',
+  signInPolicy: 'B2C_1_react_test_signup',
+  applicationId: 'a239d3ec-f2e3-4b55-b674-ed2a7c672620',
+  cacheLocation: 'sessionStorage',
+  scopes: ['https://cmapp2.onmicrosoft.com/api/user_impersonation'],
+  redirectUri: 'http://localhost:3000',
+  postLogoutRedirectUri: window.location.origin,
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+b2cauth.run(() => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+  serviceWorker.unregister();
+});
